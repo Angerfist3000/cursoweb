3d40fba5de09cf8a9103e9c18ab6edc427340094
Angerfist3000 <105991160+Angerfist3000@users.noreply.github.com>
2022-07-06 20:16:51 -0500

    Unidad III. Tarea 2

---
+++ archivo2.php
@@ -0,0 +1,56 @@
+<html>
+<body>
+
+<?php
+//SUMA DE DOS ENTEROS
+$num1 = 4;
+$num2 = 6;
+$numres = $num1 + $num2;
+
+$num1 + $num2;
+echo "la suma es:$numres";
+
+echo "<br>";
+echo "<br>";
+
+// SUMA DE DOS DECIMALES
+
+$num1 = 12.5;
+$num2 = 3.9;
+$numres = $num1 + $num2;
+
+$num1 + $num2;
+echo "la suma es:$numres";
+
+echo "<br>";
+echo "<br>";
+
+
+//MULTIPLICACION DE DOS NUMEROS
+
+$num1 = 4;
+$num2 = 5;
+$numres = $num1 * $num2;
+
+$num1 * $num2;
+echo "la multiplicacion es:$numres";
+
+echo "<br>";
+echo "<br>";
+
+
+//CONCATENACION DE PALABRAS
+
+$nombre = "German";
+$edad = "20" ;
+$concatenacion = $nombre . $edad ;
+echo $concatenacion;
+
+
+
+
+
+?> 
+
+</body>
+</html>
